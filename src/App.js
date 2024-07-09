b8501f524fdd4115fb70c953053fa8e37e77b376
mizik1 <mattk29@gmail.com>
2024-07-09 12:58:18 -0500

    Changed from 'mainview' to 'main-view'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { MainView } from "./components/main-view/mainview";
+import { MainView } from "./components/main-view/main-view";
 
 function App() {
   return (
