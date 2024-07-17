0161b944f9223fcbc15a5a09d3956344c4162eb7
mizik1 <mattk29@gmail.com>
2024-07-16 19:26:22 -0500

    Created new branch 'movie-data-react'
    Replace the hardcoded movies array with an empty array as the initial value. Populated movies state using setMovies.

--- src/components/moviecard-view/moviecard-view.jsx
+++ src/components/moviecard-view/moviecard-view.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div onClick={() => onMovieClick(movie)}>
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      <img src="{movie.imageURL" alt="{movie.Title" />
       <h2>{movie.Title}</h2>
+      <p>{movie.Description}</p>
     </div>
   );
+};
+
+// Prop types
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.string.isRequired,
+    Director: PropTypes.string.isRequired,
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    BirthDate: PropTypes.string.isRequired,
+    imageURL: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+    movieid: PropTypes.number.isRequired,
+    onMovieClick: PropTypes.func.isRequired,
+  }),
 };
