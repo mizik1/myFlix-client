7fb71cebe9e2fd7c997e40e798363ae4129cd5cc
mizik1 <mattk29@gmail.com>
2024-07-30 11:29:40 -0500

    started login view
    Added login view and connected to 'movie_api'

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+    };
+
+    // Sends post request to my API using fetch then response is parsed to JSON with token
+    fetch("https://great-movies-flix-ecc6317feb54.herokuapp.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json()) // Corrected to .json()
+      .then((data) => {
+        if (data.user) {
+          // Call the onLoggedIn function passed on as a prop
+          onLoggedIn(data.user, data.token);
+        } else {
+          // Handle login failure
+          setError("Login failed. Please check your credentials.");
+        }
+      })
+      // catch any errors that happened during the fetch request
+      .catch((error) => {
+        setError("An error occurred. Please try again later.");
+      });
+  };
+
+  // handle form submission. Username input and Password input
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <p className="error">{error}</p>}
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
